2f94179adf4c18feca1d178a6137b1a08f72ffd1
danixoid <danixoid@gmail.com>
2017-07-04 17:03:10 +0600

    seed db org positions

--- database/seeds/PositionTableSeeder.php
+++ database/seeds/PositionTableSeeder.php
@@ -11,14 +11,62 @@ class PositionTableSeeder extends Seeder
      */
     public function run()
     {
-        foreach(\App\User::all() as $user) :
+        $poses = [
+            'deps' => [
+                'Директор',
+                'Заместитель директора',
+            ],
+            'funct' => [
+                'Руководитель группы',
+                'Главный менеджер',
+                'Главный менеджер 1 уровня',
+                'Главный менеджер 2 уровня',
+                'Главный менеджер 3 уровня',
+                'Менеджер',
+                'Менеджер 1 уровня',
+                'Менеджер 2 уровня',
+                'Менеджер 3 уровня',
+                'Начальник управления',
+                'Начальник отдела',
+                'Руководитель',
+                'Главный специалист',
+                'Эксперт',
+            ],
+        ];
 
-            $org = \App\Org::inRandomOrder()->first();
-            $position = new \App\Position();
-            $position->name = 'Должность ' . $user->id;
-            $position->org_id = $org->id;
-            $position->save();
-        endforeach;
+//        var_dump($poses);
+        
+        $roots = \App\Org::whereNull('org_id')->first()->children;
+
+        foreach ($roots as $root) {
+            foreach ($root->children as $sub) {
+//                echo $sub->name;
+                foreach ($poses['deps'] as $pos) {
+                    $position = new \App\Position();
+                    $position->name = $pos;
+                    $position->org_id = $sub->id;
+                    $position->save();
+                }
+
+                foreach ($sub->children as $child) {
+                    foreach ($poses['funct'] as $post) {
+                        $position = new \App\Position();
+                        $position->name = $post;
+                        $position->org_id = $child->id;
+                        $position->save();
+                    }
+                }
+            }
+        }
+
+//        foreach(\App\User::all() as $user) :
+//
+//            $org = \App\Org::inRandomOrder()->first();
+//            $position = new \App\Position();
+//            $position->name = 'Должность ' . $user->id;
+//            $position->org_id = $org->id;
+//            $position->save();
+//        endforeach;
     }
 
 }
